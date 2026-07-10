b399eaf9600b1def2529bd51f2f21a130f676b69
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2026-07-10 14:18:58 +0530

    Remove S3 backend configuration
    Removed S3 backend configuration from Terraform.

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket       = "s3-backend-2026"
-    key          = "terraform-aws/terraform.tfstate"
-    region       = "ap-southeast-2"
-    use_lockfile = true
-  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
